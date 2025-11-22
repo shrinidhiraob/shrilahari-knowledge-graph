6f3c40b7b4350c3b282c96eed7b0173ccda8c03e
shrinidhiraob <41549487+shrinidhiraob@users.noreply.github.com>
2025-11-22 19:29:46 +0530

    Update shrilahari-knowledge-graph.php

--- shrilahari-knowledge-graph.php
+++ shrilahari-knowledge-graph.php
@@ -26,6 +26,8 @@ function sh_kg_load_modules() {
     require_once SH_KG_PLUGIN_DIR . 'modules/temple/temple-ui.php';
     require_once SH_KG_PLUGIN_DIR . 'modules/frontend/breadcrumbs.php';
     require_once SH_KG_PLUGIN_DIR . 'modules/frontend/faq-schema.php';
+    require_once SH_KG_PLUGIN_DIR . 'modules/core/map-picker.php';
+
 
 
 
