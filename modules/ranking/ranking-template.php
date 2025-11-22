3d29f9b4d54107c5c0ea1293374436825d9fcb97
shrinidhiraob <41549487+shrinidhiraob@users.noreply.github.com>
2025-11-22 23:11:09 +0530

    Delete modules directory

--- modules/ranking/ranking-template.php
+++
@@ -1,8 +0,0 @@
-<?php /* Template Name: Temple Ranking Template */ ?>
-<?php get_header(); ?>
-<div class="sh-ranking-full">
-    <main class="site-main">
-        <?php while ( have_posts() ) : the_post(); the_content(); endwhile; ?>
-    </main>
-</div>
-<?php get_footer(); ?>
